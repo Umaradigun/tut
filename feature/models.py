38804ed948e109e6d1d66aa63c9758ca5816429e
Umaradigun <adigunfaruk09061@gmail.com>
2024-03-17 23:34:51 -0400

    Second commit

--- feature/models.py
+++ feature/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class (models.Model):
+
+
+    class Meta:
+        verbose_name = _("")
+        verbose_name_plural = _("s")
+
+        title = models.CharField(max_length= 100),
+        authors_name= models.CharField(max_length=50),
+        ratings= models.IntegerField(default=True),
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
